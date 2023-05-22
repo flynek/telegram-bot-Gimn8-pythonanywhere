58a0801703dfbbf5bb08bc683f95b37108e47ab1
Timofey Litovchuk <125207400+flynek@users.noreply.github.com>
2023-05-22 22:19:37 +0300

    Update clients.py

--- Gimn8Bot/handlers/clients.py
+++ Gimn8Bot/handlers/clients.py
@@ -28,62 +28,78 @@ async def command_start(message: types.Message):
 previous_kb = InlineKeyboardMarkup
 previous_message = ''
 ultimate_caption = 'Для получения информации скачайте документ'
+back_counter = 0
+kb_dict = [kb_start, kb_parents, kb_course]
+message_dict = [dict_messages['st_m'], dict_messages['pr_m'], dict_messages['cr_m']]
+
 
 @dp.callback_query_handler(text_startswith=['st_', 'pr_', 'cr_', 'back'])
 async def start_func(callback: types.CallbackQuery):
-    global previous_kb, previous_message
+    global previous_kb, previous_message, back_counter
     image_sign = InputMediaPhoto(open('photos\\sign.jpg', 'rb'))
-    if callback.data[:3] == 'st_':
-        previous_kb = kb_start
-        previous_message = dict_messages['st_m']
-        if callback.data == 'st_holidays':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('hol_m'), kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'st_director':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('dr_m'), kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'st_address':
-            image = InputMediaPhoto(open('photos\\address_photo.jpg', 'rb'))
-            await edit_message_photo(call=callback, photo=image, caption=dict_messages['add_m'], kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'st_new_kb_parents':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('pr_m'), kb=kb_parents)
-            await callback.answer()
-    elif callback.data[:3] == 'pr_':
-        previous_kb = kb_parents
-        previous_message = dict_messages['pr_m']
-        if callback.data == 'pr_class_1_doc':
-            doc = InputMediaDocument(open('files\\О_приёме_на_обучение.pdf', 'rb'))
-            await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'pr_uniform_doc':
-            doc = InputMediaDocument(open('files\\О_школьной_форме.pdf', 'rb'))
-            await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'pr_new_kb_course':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('cr_m'), kb=kb_course)
-            await callback.answer()
-    elif callback.data[:3] == 'cr_':
-        previous_kb = kb_course
-        previous_message = dict_messages['cr_m']
-        if callback.data == 'cr_register_course':
-            doc = InputMediaDocument(open('files\\Договор_Дошкольник.docx', 'rb'))
-            await edit_message_doc(call=callback, doc=doc, caption=dict_messages.get('reg_cr_m'), kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'cr_program_course':
-            doc = InputMediaDocument(open('files\\Программа_Дошкольник.pdf', 'rb'))
-            await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'cr_open_course':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('o_cr_m'), kb=kb_back)
-            await callback.answer()
-        elif callback.data == 'cr_pay_course':
-            await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('p_cr_m'), kb=kb_back)
-            await callback.answer()
-    else:
+    if callback.data == 'back':
+        back_counter += 1
+        if back_counter >= 2:
+            index = kb_dict.index(previous_kb)
+            previous_kb = kb_dict[index-1]
+            previous_message = message_dict[index-1]
         await edit_message_photo(call=callback, photo=image_sign, caption=previous_message, kb=previous_kb)
         await callback.answer()
+    else:
+        back_counter = 0
+        if callback.data[:3] == 'st_':
+            previous_kb = kb_start
+            previous_message = dict_messages['st_m']
+            if callback.data == 'st_holidays':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('hol_m'),
+                                         kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'st_director':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('dr_m'), kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'st_address':
+                image = InputMediaPhoto(open('photos\\address_photo.jpg', 'rb'))
+                await edit_message_photo(call=callback, photo=image, caption=dict_messages['add_m'], kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'st_new_kb_parents':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('pr_m'),
+                                         kb=kb_parents)
+                await callback.answer()
+        elif callback.data[:3] == 'pr_':
+            previous_kb = kb_parents
+            previous_message = dict_messages['pr_m']
+            if callback.data == 'pr_class_1_doc':
+                doc = InputMediaDocument(open('files\\О_приёме_на_обучение.pdf', 'rb'))
+                await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'pr_uniform_doc':
+                doc = InputMediaDocument(open('files\\О_школьной_форме.pdf', 'rb'))
+                await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'pr_new_kb_course':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('cr_m'),
+                                         kb=kb_course)
+                await callback.answer()
+        elif callback.data[:3] == 'cr_':
+            previous_kb = kb_course
+            previous_message = dict_messages['cr_m']
+            if callback.data == 'cr_register_course':
+                doc = InputMediaDocument(open('files\\Договор_Дошкольник.docx', 'rb'))
+                await edit_message_doc(call=callback, doc=doc, caption=dict_messages.get('reg_cr_m'), kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'cr_program_course':
+                doc = InputMediaDocument(open('files\\Программа_Дошкольник.pdf', 'rb'))
+                await edit_message_doc(call=callback, doc=doc, caption=ultimate_caption, kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'cr_open_course':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('o_cr_m'),
+                                         kb=kb_back)
+                await callback.answer()
+            elif callback.data == 'cr_pay_course':
+                await edit_message_photo(call=callback, photo=image_sign, caption=dict_messages.get('p_cr_m'),
+                                         kb=kb_back)
+                await callback.answer()
 
-        
+
 def register_handlers_clients(dispatcher: Dispatcher):
     dispatcher.register_message_handler(command_start, commands=['start', 'help'])
